eda4ca98c211b5f0cb0282eef606346a9b7259b8
AsetK <41279619+AsetK@users.noreply.github.com>
2019-08-29 16:53:04 +0600

    20-commit: from web

--- src/main/java/com/epam/git/Main.java
+++ src/main/java/com/epam/git/Main.java
@@ -21,6 +21,7 @@ public class Main {
         System.out.println("17");
         System.out.println("18-from web");
         System.out.println("19");
+        System.out.println("20-from web");
 
 
     }
